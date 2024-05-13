f03d8fa5049665d60423593b9d76535578821e6c
Matthew Frantz <mcfrantz12@gmail.com>
2024-05-13 12:42:36 -0400

    File Changes

--- gpu_test.py
+++
@@ -1,7 +0,0 @@
-import torch
-print("CUDA available:", torch.cuda.is_available())
-print("Number of GPUs:", torch.cuda.device_count())
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-tensor = torch.tensor([1, 2, 3], device=device)
-print(tensor)
-
